59bb8c71e420c5b48df9723dd4449e051630f90f
jhonthe77 <jhonthe7@gmail.com>
2023-01-24 09:58:43 -0500

    cambie el tipo de autenticacion form

--- tasks/views.py
+++ tasks/views.py
@@ -49,11 +49,11 @@ def singout(request):
 def signin(request):
     if request.method == 'GET':
         return render(request, 'signin.html', {
-            'form': UserCreationForm
+            'form': AuthenticationForm
         })
     else:
         user=authenticate(
-            request, username=request.POST['username'], password=request.POST['password1'])
+            request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'signin.html', {
                 'from': AuthenticationForm,
