7d6ad3c647b0900438de5dd3070f4563ac0976f2
jhonthe77 <jhonthe7@gmail.com>
2023-01-24 08:37:06 -0500

    agrego  git ignore

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.http import HttpResponse
 
 
@@ -38,3 +38,12 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'task.html')
+
+def signupout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    return render(request, 'signin.html',{
+        'from': AuthenticationForm
+    })
